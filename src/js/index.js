88e0ea05dffb6a7f387af185f3f7e2b6c50c7e87
cindon <nicolascindon@gmail.com>
2023-12-05 17:31:53 +0100

    search css modified and js added + card color modified

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import {dropdown} from "./components/dropdown.js";
+import {search} from "./components/search.js";
 
 function init() {
     console.log("init");
     dropdown();
+    search();
 }
 
 init()
